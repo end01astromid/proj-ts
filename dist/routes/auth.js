94fdbbac67e47bd2d6bdf10b41163f16ce064697
Sultan <astromidand2929@gmail.com>
2026-02-15 14:11:41 +0500

    Revert "поменял  код на промисах в /come-in на async/await"
    This reverts commit 1fb581a7ef4f3bd28790ffc8786c95203a9a4afb.

--- dist/routes/auth.js
+++ dist/routes/auth.js
@@ -18,33 +18,37 @@ router.post('/register', async (req, res) => {
         // Создаём нового пользователя
         const newUser = new User({ username, email, password: hashedPassword });
         await newUser.save();
-        res.json({ message: "добро пожаловать" });
+        res.json(newUser);
     }
     catch (err) {
         console.error("Ошибка регистрации:", err);
         res.status(500).json({ message: "Ошибка сервера" });
     }
 });
-router.post('/come-in', async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        if (!email || !password) {
-            return res.status(400).json({ message: "заполните поле" });
-        }
-        const user = await User.findOne({ email });
+//код на промисах
+//заменить лучшее async/await
+router.post('/come-in', (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json({ message: "заполните поле" });
+    }
+    User.findOne({ email })
+        .then((user) => {
         if (!user) {
-            return res.status(400).json({ message: "Неверный email или пароль" });
+            throw { status: 400, message: "Пользователь не найден" };
         }
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            return res.status(400).json({ message: 'Неверный email или пароль' });
-        }
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
-        return res.status(200).json({ token, user: { id: user._id, email: user.email } });
-    }
-    catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Ошибка сервера" });
-    }
+        return Promise.all([user, bcrypt.compare(password, user.password)])
+            .then(([user, isPasswordValid]) => {
+            if (!isPasswordValid) {
+                throw { status: 401, message: "Неверный пароль" };
+            }
+            const token1 = jwt.sign({ userId: user._id, email: user.email }, process.env.JWT_SECRET || "JWT_SECRET", { expiresIn: "24h" });
+            res.json({ message: "Успешный вход", token1 });
+        });
+    })
+        .catch((err) => {
+        const status = err.status || 500;
+        res.status(status).json({ message: err.message || "Ошибка сервера" });
+    });
 });
 export default router;
