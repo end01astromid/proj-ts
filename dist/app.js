b8d485d5ff5b618de365da5eb392fa915d55a800
Sultan <astromidand2929@gmail.com>
2026-02-15 13:15:33 +0500

    убрал коментарий промисов

--- dist/app.js
+++ dist/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import authRouter from './routes/auth.js';
+import 'dotenv/config';
 const app = express();
 app.use(express.json());
 app.use(cors());
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -20,7 +20,7 @@ router.post('/register',async(req:Request, res: Response)=>{
          // Создаём нового пользователя
          const newUser = new User({username,email, password: hashedPassword})
          await newUser.save()               
-         res.json(newUser)
+         res.json({message: "добро пожаловать"})
     }catch(err){
         console.error("Ошибка регистрации:", err);
         res.status(500).json({ message: "Ошибка сервера" });
@@ -36,60 +36,26 @@ router.post('/come-in',async (req: Request, res: Response)=>{
      }
      const user = await User.findOne({email})
      if(!user){
-     return res.status(400).json({ message: "Пользователь уже существует" });
- }   
+      return res.status(400).json({ message: "Неверный email или пароль" });
+ }  
  const isMatch = await bcrypt.compare(password,user.password)
    if(!isMatch){
     return res.status(400).json({ message: 'Неверный email или пароль'});
 }
+
     const token = jwt.sign(
         {id: user._id},
         process.env.JWT_SECRET as string,
         {expiresIn: '24h'}
 )
    return res.status(200).json({token, user: {id: user._id, email: user.email}})
+
 }catch(error){
      console.error(error);
     res.status(500).json({ message: "Ошибка сервера" });
   }
 })
 
-
-
-
- 
-// //код на промисах
-// //заменить лучшее async/await
-// router.post('/come-in', (req:Request, res:Response)=>{
-//     const {email, password} = req.body
-//     if (!email || !password) {
-//         return res.status(400).json({ message: "заполните поле" })
-//     }
-
-//     User.findOne({email})
-//     .then((user)=>{
-//          if (!user) {
-//          throw { status: 400, message: "Пользователь не найден" }
-//       }
-//       return Promise.all([user,bcrypt.compare(password, user.password)])
-//       .then(([user, isPasswordValid])=>{
-//         if(!isPasswordValid){
-//          throw { status: 401, message: "Неверный пароль" }
-//         }
-
-//         const token1 = jwt.sign(
-//             {userId: user._id, email: user.email},
-//             process.env.JWT_SECRET || "JWT_SECRET",
-//             {expiresIn: "24h"}
-//         )       
-//          res.json({ message: "Успешный вход", token1 })
-//       })
-//     })
-//     .catch((err)=>{
-//      const status = err.status || 500
-//      res.status(status).json({ message: err.message || "Ошибка сервера" }
-//     )})
-// })
 
 
 
